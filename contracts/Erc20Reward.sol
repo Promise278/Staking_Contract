38abfd829865f9ced6a9a04a1b5ffe2dce29369c
Promise278 <promiseobi2007@gmail.com>
2026-05-21 11:37:51 +0100

    fixs: updated erc20rewardstoken

--- contracts/Erc20Reward.sol
+++ contracts/Erc20Reward.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
-
 import "./Erc20Token.sol";
-
 contract Erc20Reward is Erc20Token {
     constructor() Erc20Token("C4 Rewards", "C4R", 0) {}
 
